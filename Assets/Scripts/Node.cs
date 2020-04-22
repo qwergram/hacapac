6f8495f6f14550f78a2308608f3a220db4deaff5
richardyjtian <richardyjtian@hotmail.com>
2020-04-22 11:30:42 -0700

    Collisions working

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    // Neighbors we can go to
+    public Node[] neighbors;
+    // Directions of neighbors
+    public Vector2[] validDirections;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        validDirections = new Vector2[neighbors.Length];
+        // For each neighbor, find the valid direction to that neighbor
+        for (int i = 0; i < neighbors.Length; i++)
+        {
+            Node neighbor = neighbors[i];
+            Vector2 tempVector = neighbor.transform.localPosition - transform.localPosition;
+            validDirections[i] = tempVector.normalized;
+        }
+    }
+}
---
+++ Assets/Scripts/PacMan.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacMan : MonoBehaviour
+{
+    public float speed = 50.0f;
+
+    Rigidbody2D rigidbody2d;
+    private Vector2 direction = Vector2.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called every frame
+    void Update()
+    {
+        CheckInput();
+        UpdateOrientation();
+        Move();
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+        } else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+    }
+
+    void Move()
+    {
+        rigidbody2d.velocity = direction * speed;
+    }
+
+    void UpdateOrientation()
+    {
+        if (direction == Vector2.left)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        } else if (direction == Vector2.right)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        } else if (direction == Vector2.up)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 90);
+        } else if (direction == Vector2.down)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            transform.localRotation = Quaternion.Euler(0, 0, 270);
+        }
+    }
+}
