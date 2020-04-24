703d7a324f1db7b3662a8209b20c1d7bb03759f1
richardyjtian <richardyjtian@hotmail.com>
2020-04-23 20:46:57 -0700

    Movement semi working

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
-    private float speed = 3.0f;
+    private float speed = 4.0f;
 
     Rigidbody2D rigidbody2d;
     private Vector2 direction = Vector2.zero;
+    // The position where the PacMan was when the user inputted a command
+    private Vector2 positionOfInput = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,6 @@ public class PacMan : MonoBehaviour
     void Update()
     {
         CheckInput();
-        UpdateOrientation();
         Move();
     }
 
@@ -29,21 +30,46 @@ public class PacMan : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
+            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction = Vector2.right;
+            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction = Vector2.up;
+            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction = Vector2.down;
+            positionOfInput = rigidbody2d.position;
         }
     }
 
     void Move()
     {
-        rigidbody2d.velocity = direction * speed;
+        if (direction == Vector2.left || direction == Vector2.right)
+        {
+            float floor_y = Mathf.Floor(positionOfInput.y);
+            float ceil_y = Mathf.Ceil(positionOfInput.y);
+            float curr_y = rigidbody2d.position.y;
+            if (curr_y - floor_y < 0.015f || ceil_y - curr_y < 0.015f)
+            {
+                rigidbody2d.velocity = direction * speed;
+                UpdateOrientation();
+            }
+        }
+        else if (direction == Vector2.up || direction == Vector2.down)
+        {
+            float floor_x = Mathf.Floor(positionOfInput.x);
+            float ceil_x = Mathf.Ceil(positionOfInput.x);
+            float curr_x = rigidbody2d.position.x;
+            if (curr_x - floor_x < 0.015f || ceil_x - curr_x < 0.015f)
+            {
+                rigidbody2d.velocity = direction * speed;
+                UpdateOrientation();
+            }
+        }
     }
 
     void UpdateOrientation()
