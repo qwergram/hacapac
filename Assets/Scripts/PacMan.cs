c25d0aa9efaa78652b9986b52a0ee7c241b1268f
richardyjtian <richardyjtian@hotmail.com>
2020-04-23 21:29:17 -0700

    Movement has been perfected

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -4,17 +4,16 @@ using UnityEngine;
 
 public class PacMan : MonoBehaviour
 {
-    private float speed = 4.0f;
+    public float moveSpeed = 4.0f;
+    public Transform movePoint;
+    public LayerMask whatStopsMovement;
 
-    Rigidbody2D rigidbody2d;
     private Vector2 direction = Vector2.zero;
-    // The position where the PacMan was when the user inputted a command
-    private Vector2 positionOfInput = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
     {
-        rigidbody2d = GetComponent<Rigidbody2D>();
+        movePoint.parent = null;
     }
 
     // Update is called every frame
@@ -30,44 +29,38 @@ public class PacMan : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             direction = Vector2.left;
-            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             direction = Vector2.right;
-            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
             direction = Vector2.up;
-            positionOfInput = rigidbody2d.position;
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
             direction = Vector2.down;
-            positionOfInput = rigidbody2d.position;
         }
     }
 
     void Move()
     {
-        if (direction == Vector2.left || direction == Vector2.right)
+        transform.position = Vector2.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            float floor_y = Mathf.Floor(positionOfInput.y);
-            float ceil_y = Mathf.Ceil(positionOfInput.y);
-            float curr_y = rigidbody2d.position.y;
-            if (curr_y - floor_y < 0.015f || ceil_y - curr_y < 0.015f)
+            if (direction == Vector2.left || direction == Vector2.right)
             {
-                rigidbody2d.velocity = direction * speed;
-                UpdateOrientation();
+                if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)direction, 0.2f, whatStopsMovement))
+                {
+                    movePoint.position += (Vector3)direction;
+                    UpdateOrientation();
+                }
             }
-        }
-        else if (direction == Vector2.up || direction == Vector2.down)
-        {
-            float floor_x = Mathf.Floor(positionOfInput.x);
-            float ceil_x = Mathf.Ceil(positionOfInput.x);
-            float curr_x = rigidbody2d.position.x;
-            if (curr_x - floor_x < 0.015f || ceil_x - curr_x < 0.015f)
+            else if (direction == Vector2.up || direction == Vector2.down)
             {
-                rigidbody2d.velocity = direction * speed;
-                UpdateOrientation();
+                if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)direction, 0.2f, whatStopsMovement))
+                {
+                    movePoint.position += (Vector3)direction;
+                    UpdateOrientation();
+                }
             }
         }
     }
