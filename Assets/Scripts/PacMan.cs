6309cb507b56254887d3a7eb37c48b4ef3c547e2
richardyjtian <richardyjtian@hotmail.com>
2020-04-23 21:46:20 -0700

    Movement has been further perfected

--- Assets/Scripts/PacMan.cs
+++ Assets/Scripts/PacMan.cs
@@ -8,7 +8,9 @@ public class PacMan : MonoBehaviour
     public Transform movePoint;
     public LayerMask whatStopsMovement;
 
-    private Vector2 direction = Vector2.zero;
+    private Vector2 newDirection = Vector2.zero;
+    // Previous direction
+    private Vector2 oldDirection = Vector2.zero;
 
     // Start is called before the first frame update
     void Start()
@@ -28,16 +30,16 @@ public class PacMan : MonoBehaviour
         // Change the direction based on input
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            direction = Vector2.left;
+            newDirection = Vector2.left;
         } else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            direction = Vector2.right;
+            newDirection = Vector2.right;
         } else if (Input.GetKeyDown(KeyCode.UpArrow))
         {
-            direction = Vector2.up;
+            newDirection = Vector2.up;
         } else if (Input.GetKeyDown(KeyCode.DownArrow))
         {
-            direction = Vector2.down;
+            newDirection = Vector2.down;
         }
     }
 
@@ -46,40 +48,39 @@ public class PacMan : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
         if (Vector2.Distance(transform.position, movePoint.position) <= 0.05f)
         {
-            if (direction == Vector2.left || direction == Vector2.right)
+            // If no collision between movePoint and grid, move the movePoint to the new location
+            if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)newDirection, 0.2f, whatStopsMovement))
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)direction, 0.2f, whatStopsMovement))
-                {
-                    movePoint.position += (Vector3)direction;
-                    UpdateOrientation();
-                }
-            }
-            else if (direction == Vector2.up || direction == Vector2.down)
+                UpdateOrientation(newDirection);
+                oldDirection = newDirection;
+                movePoint.position += (Vector3)newDirection;
+            } 
+            // Else if no collision if keep going the old way, then keep going the old way
+            else if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)oldDirection, 0.2f, whatStopsMovement))
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + (Vector3)direction, 0.2f, whatStopsMovement))
-                {
-                    movePoint.position += (Vector3)direction;
-                    UpdateOrientation();
-                }
+                movePoint.position += (Vector3)oldDirection;
             }
         }
     }
 
-    void UpdateOrientation()
+    void UpdateOrientation(Vector2 direction)
     {
         if (direction == Vector2.left)
         {
             transform.localScale = new Vector3(-1, 1, 1);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
-        } else if (direction == Vector2.right)
+        }
+        else if (direction == Vector2.right)
         {
             transform.localScale = new Vector3(1, 1, 1);
             transform.localRotation = Quaternion.Euler(0, 0, 0);
-        } else if (direction == Vector2.up)
+        }
+        else if (direction == Vector2.up)
         {
             transform.localScale = new Vector3(1, 1, 1);
             transform.localRotation = Quaternion.Euler(0, 0, 90);
-        } else if (direction == Vector2.down)
+        }
+        else if (direction == Vector2.down)
         {
             transform.localScale = new Vector3(1, 1, 1);
             transform.localRotation = Quaternion.Euler(0, 0, 270);
