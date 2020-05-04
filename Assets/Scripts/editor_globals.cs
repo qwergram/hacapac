e43186a892808dd1cb00bf5777a900ca550f779a
patrick <patrick@adaptilab.com>
2020-05-04 11:19:10 -0700

    Initial commit: button logic, selection. TODO: add spawning on click, cursor tool follow

---
+++ Assets/Scripts/editor_globals.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class editor_globals : MonoBehaviour
+{
+    private string selected_tool = "none";
+    private Sprite selected_sprite = null;
+
+    public string getSelectedTool() {
+        return selected_tool;
+    }
+
+    public void set_selected_tool(string new_tool, Sprite new_sprite) {
+        selected_tool = new_tool;
+        selected_sprite = new_sprite;
+        Debug.Log(new_tool);
+        Debug.Log(new_sprite);
+    }
+}
