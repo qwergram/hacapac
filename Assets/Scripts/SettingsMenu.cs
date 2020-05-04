2c0f50227e925fa77ce6c8a76b61eff9a1e4fb25
Norton Pengra <npengra317@gmail.com>
2020-05-04 09:43:18 -0700

    add volume options

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -7,8 +7,15 @@ public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
 
+    private float _volume;
+    public float Volume {
+        get { return _volume; }
+        private set { _volume = value; }
+    }
+
     public void SetVolume (float volume)
     {
+        Volume = volume;
         audioMixer.SetFloat("MasterVolume", volume);
     }
 }
