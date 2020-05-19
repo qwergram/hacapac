96f723d021a593166824e9caaca33a5290427404
richardyjtian <richardyjtian@hotmail.com>
2020-05-19 10:26:33 -0700

    Added button to editor

--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -8,6 +8,6 @@ public class GameMenu : MonoBehaviour
     public void Back()
     {
         Destroy(GameObject.FindGameObjectWithTag("Game"));
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        SceneManager.LoadScene(0);
     }
 }
